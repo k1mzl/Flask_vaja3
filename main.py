1ab4ed84a4a5a179e056330e90beef2dfd26b3a3
k1mzl <oblakkim25@gmail.com>
2026-02-25 07:49:33 +0000

    aha

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template, 
 #import requests
 import random
 #pip install flask
@@ -14,8 +14,17 @@ def coinflip():
 
 @app.route("/coinData")
 def coinData():
+
+
+    #print(request.args["vrednost"])    
     rnd = random.randint(0,1)
-    return["TAILS","HEADS"][rnd]
+    status = ["TAILS", "HEADS"][rnd]
+    if status == "HEADS":
+        url = "https://i.postimg.cc/CBNJNfDJ/head.png"
+    else:
+        url = "https://i.postimg.cc/zysdXN8w/tail.png" 
+
+    return {"img": url, "status": status}
 
     return render_template("coin.html")
 
@@ -26,10 +35,23 @@ def randomQuote():
 
 @app.route("/Quotedata")
 def Quotedata():
-   
-    rnd = random.randint(0,1)
-    return["TAILS","HEADS"][rnd]
+    quotes = [
+        {"quote": "Life is what happens when you’re busy making other plans.", "author": "John Lennon"},
+        {"quote": "The only way to do great work is to love what you do.", "author": "Steve Jobs"},
+        {"quote": "The journey of a thousand miles begins with one step.", "author": "Lao Tzu"},
+        {"quote": "Believe you can and you’re halfway there.", "author": "Theodore Roosevelt"},
+        {"quote": "You miss 100% of the shots you don’t take.", "author": "Wayne Gretzky"},
+        {"quote": "Don’t count the days, make the days count.", "author": "Muhammad Ali"},
+        {"quote": "Be the change that you wish to see in the world.", "author": "Mahatma Gandhi"},
+        {"quote": "What you get by achieving your goals is not as important as what you become by achieving your goals.", "author": "Zig Ziglar"},
+        {"quote": "It does not matter how slowly you go as long as you do not stop.", "author": "Confucius"},
+    {   "quote": "Success is not final, failure is not fatal: it is the courage to continue that counts.", "author": "Winston Churchill"}
+    
+    ]   
 
-    return render_template("quote.html")
+    izbran = random.choice(quotes)
+    return izbran
+
+
 
 app.run(debug = True)
